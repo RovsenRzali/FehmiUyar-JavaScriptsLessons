db5fe16fff02e9847e0788c0ae21d0db5540cc9f
RovsenRzali <rovsen_204@mail.ru>
2022-09-13 18:51:01 +0400

    Add lesson53

---
+++ lesson53/app.js
@@ -0,0 +1,22 @@
+document.querySelector("#result").innerHTML =
+(0 | 0) +
+"<br>" +
+(0 | 1) +
+"<br>" +
+(1 | 0) +
+"<br>" +
+(1 | 1) +
+"<br>" +
+(1111 | 0000) +
+"<br>" +
+(1111 | 0001) +
+"<br>" +
+(1111|0010)  +
+"<br>" +
+(1111 | 0100);
+
+
+/* " | "  bu operatoru toplama əməli kimi fikirləşin. */
+
+
+
