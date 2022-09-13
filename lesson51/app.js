91522a91a0f7c7c782553ddbbc2efe1d5a9e521a
RovsenRzali <rovsen_204@mail.ru>
2022-09-13 15:44:08 +0400

    Add lesson51

---
+++ lesson51/app.js
@@ -0,0 +1,15 @@
+document.querySelector('#result').innerHTML = 
+(0|0) + "<br>" +
+(0|1) + "<br>" +
+(1|1) + "<br>"+ 
+(0&0) + "<br>" +
+(0&1) + "<br>" +
+(1&1)+ "<br>" +
+"XOR ^ operatoru:" + "<br>" +
+(0^0) + "<br>" +
+(0^1) + "<br>" +
+(1^1);
+
+document.querySelector('#result2').innerHTML = "NOT ~ bitwise operatoru:" + ~4;
+ 
+document.querySelector('#result2').innerHTML = "NOT ~ bitwise operatoru:" + ~4
