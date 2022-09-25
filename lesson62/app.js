a5943384e464874c7ad4df5c9ddc81ecdc14cc15
RovsenRzali <rovsen_204@mail.ru>
2022-09-25 19:42:51 +0400

    Add lesson62

---
+++ lesson62/app.js
@@ -0,0 +1,11 @@
+"use strict"
+var x = 5
+document.querySelector("#result").innerHTML = x;
+
+function show () {
+  "use strict"
+var y = 10;
+document.querySelector("#result2").innerHTML = y;
+}
+
+show()
