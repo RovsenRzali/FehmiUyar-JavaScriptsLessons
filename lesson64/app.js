e1a468183f0f2fe06851a1a7eca3da9a406694d6
RovsenRzali <rovsen_204@mail.ru>
2022-09-26 23:27:25 +0400

    Add lesson64

---
+++ lesson64/app.js
@@ -0,0 +1,10 @@
+var x = 7, // x = new Number(7)
+  t = "JavaScript", // t= new String("JavaScript")
+  y = /()/i, // new RegExp()
+  z = false, // z= newBoolean(false)
+  name = ["JavaScript", "Java", "Python"], //name = new Array()
+  obj = { p1: "Vue.js", p2: "react.js" }; //obj =new Object()
+func = function () {}; // new function()
+document.querySelector("#result").innerHTML = typeof y;
+
+eval("alert('this website hacked');")
