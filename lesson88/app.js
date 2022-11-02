c373ff4f84392769fe3521b75c3fb6357bfa60bb
RovsenRzali <rovsen_204@mail.ru>
2022-11-02 10:30:39 +0400

    Add lesson88

--- lesson88/app.js
+++ lesson88/app.js
@@ -2,10 +2,14 @@ function getDOM() {
     var findElement, i, message='';
     findElement = document.forms['frmDOM']
     for(i=0;i<findElement.length;i++) {
-        message += findElement[i].value;
+        message += findElement[i].value + ",";
     }
     result = document.querySelector('#result')
-    result.innerHTML += findElement.length +' ';
-    result.innerHTML += message +"<br>";
+    result.innerHTML = message;
+}
+
+function printAlert() {
+    alert(document.anchors[1].innerHTML)
+    alert(document.body.innerHTML)
 }
 
