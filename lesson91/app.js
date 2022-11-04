2842a922753db01087b44ffe9f8cc42849bfb24e
RovsenRzali <rovsen_204@mail.ru>
2022-11-04 18:17:08 +0400

    Add lesson91

--- lesson91/app.js
+++ lesson91/app.js
@@ -5,7 +5,7 @@ function checkCookies() {
   } else {
     messages = "Cookies is active";
   }
-  alert(messages)
+  alert(messages);
 }
 
 function creatHead(par1, iner, par2) {
@@ -23,3 +23,41 @@ window.onload = function () {
   obj.innerHTML = "Javascript study";
   obj.style.fontSize = "39px";
 };
+
+function creatDiv(par1, iner) {
+  var getDiv = document.createElement("div");
+  getDiv.id = par1;
+  getDiv.innerHTML = iner;
+  getDiv.className = "container-fluid mt-3";
+  getDiv.style.border = "1px solid";
+  getDiv.style.width = "280px";
+  getDiv.style.height = "380px";
+  getDiv.style.overflow = "scroll";
+  document.querySelector("#container").appendChild(getDiv);
+}
+
+// function creatp(par1, iner, par2) {
+//   var getp = document.createElement("p");
+//   getp.id = par1;
+//   getp.innerHTML = iner;
+//   getp.className = par2;
+//   document.querySelector("#demo").appendChild(getp);
+// }
+
+function creatHead2(par1, iner, par2) {
+  var getHead2 = document.createElement("h2");
+  getHead2.id = par1;
+  getHead2.innerHTML = iner;
+  getHead2.className = par2;
+  document.querySelector("#demo").appendChild(getHead2);
+}
+
+creatDiv("demo", "test");
+var x = 0;
+document.querySelector("#demo").onscroll = function () {
+  document.querySelector("#result5").innerHTML = x++;
+};
+
+for (i = 0; i < 10; i++) {
+  creatHead2("result3", `Test${i + 1}`);
+}
